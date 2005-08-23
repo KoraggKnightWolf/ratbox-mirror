6cbb308deebac1f184c94a2cb3a1491033812dca
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-08-23 00:32:03 +0000

    forgot to add main.c oops
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@20652 b93f080a-4cfa-0310-a19a-80a68f531ef9

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c: stub main program
+ *
+ * There is a good point to this.  On platforms where shared libraries cannot
+ * have unresolved symbols, we solve this by making the core of the ircd itself
+ * a shared library.  Its kinda funky, but such is life
+ */
+int ratbox_main(int, char **);
+
+int main(int argc, char **argv)
+{
+	return ratbox_main(argc, argv);
+}
+
