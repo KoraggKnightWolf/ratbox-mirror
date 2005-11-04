a6b625d86b489350062ab583ed756bdddb0416aa
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-11-04 19:05:58 +0000

    __builtin_expect doesn't exist on gcc 2.95 and earlier
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@21068 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- libircd/ircd_lib.h
+++ libircd/ircd_lib.h
@@ -40,6 +40,10 @@
 #undef unlikely
 #endif
 
+#if __GNUC__ == 2 && __GNUC_MINOR__ < 96 
+# define __builtin_expect(x, expected_value) (x)
+#endif
+
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
 #else
