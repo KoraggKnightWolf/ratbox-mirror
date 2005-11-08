23da04c9f1a1bfbebd4f3daab4a3101ae2cbb902
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-11-08 16:41:21 +0000

    change res.h in header to dns.h
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@21106 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/dns.h
+++ include/dns.h
@@ -1,6 +1,6 @@
 /*
  *  ircd-ratbox: A slightly useful ircd.
- *  res.h: A header with the DNS functions.
+ *  dns.h: A header with the DNS functions.
  *
  *  Copyright (C) 1990 Jarkko Oikarinen and University of Oulu, Co Center
  *  Copyright (C) 1996-2002 Hybrid Development Team
