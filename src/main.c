9d7bc2e707bdd531ebcf6b7096be76ec2b57092c
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-11-21 15:38:04 +0000

    Update boilerplate on main.c
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@21261 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/main.c
+++ src/main.c
@@ -1,11 +1,33 @@
 /*
- * main.c: stub main program
+ *  ircd-ratbox: A slightly useful ircd.
+ *  main.c: Stub main program
  *
- * There is a good point to this.  On platforms where shared libraries cannot
- * have unresolved symbols, we solve this by making the core of the ircd itself
- * a shared library.  Its kinda funky, but such is life
+ *  Copyright (C) 2005 Aaron Sethman <androsyn@ratbox.org>
+ *  Copyright (C) 2005 ircd-ratbox development team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *  USA
+ *
+ *  $Id$
+ *
+ *  There is a good point to this.  On platforms where shared libraries cannot
+ *  have unresolved symbols, we solve this by making the core of the ircd itself
+ *  a shared library.  Its kinda funky, but such is life
+ *
  */
-#include <stdio.h>
+
 int ratbox_main(int, char **);
 
 int main(int argc, char **argv)
