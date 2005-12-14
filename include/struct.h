344c1c68d3ec637db96cfa0dda6df5cf7069159c
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-12-14 20:27:18 +0000

    make that uint8_t not u_int8_t
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@21498 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/struct.h
+++ include/struct.h
@@ -220,7 +220,7 @@ struct LocalUser
 
 	/* target change stuff */
 	void *targets[10];		/* targets were aware of */
-	u_int8_t targinfo[2];	/* cyclic array, no in use */
+	uint8_t targinfo[2];	/* cyclic array, no in use */
 	time_t target_last;		/* last time we cleared a slot */
 };
 
