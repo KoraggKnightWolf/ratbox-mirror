d6a3d074e5a7bee3c00ab35138f216fa7553e6c0
jilles <jilles@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2006-01-14 00:38:40 +0000

    Forward port from RATBOX_2_1: r20894 and r20664: fix possibility of RPL_WHOISCHANNELS being cut when it's sent over TS6
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@21672 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -242,6 +242,7 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
 	const char *name;
 	char quest[] = "?";
 	int visible;
+	int extra_space = 0;
 
 	if(target_p->name)
 		name = quest;
@@ -263,6 +264,17 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
 	cur_len = mlen = ircd_sprintf(buf, form_str(RPL_WHOISCHANNELS), 
 				    get_id(&me, source_p), get_id(source_p, source_p), 
 				    target_p->name);
+	/* Make sure it won't overflow when sending it to the client
+	 * in full names; note that serverhiding may require more space
+	 * for a different server name (not done here) -- jilles */
+	if (!MyConnect(source_p))
+	{
+		extra_space = strlen(source_p->name) - 9;
+		if (extra_space < 0)
+			extra_space = 0;
+		extra_space += strlen(me.name) - 2; /* make sure >= 0 */
+		cur_len += extra_space;
+	}
 
 	t = buf + mlen;
 
@@ -278,7 +290,7 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
 			if((cur_len + strlen(chptr->chname) + 3) > (BUFSIZE - 5))
 			{
 				sendto_one_buffer(source_p, HOLD_QUEUE, buf);
-				cur_len = mlen;
+				cur_len = mlen + extra_space;
 				t = buf + mlen;
 			}
 
@@ -291,7 +303,7 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
 		}
 	}
 
-	if(cur_len > mlen)
+	if(cur_len > mlen + extra_space)
 		sendto_one_buffer(source_p, HOLD_QUEUE, buf);
 
 	sendto_one_numeric(source_p, HOLD_QUEUE, RPL_WHOISSERVER, form_str(RPL_WHOISSERVER),
