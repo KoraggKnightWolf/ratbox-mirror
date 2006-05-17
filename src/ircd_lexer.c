d74bc845adca66ebeac00dadec18f88e512b08a6
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2006-05-17 18:21:19 +0000

    you know you are having a slow day when you update FSF's address on everything...
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@22567 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd_lexer.c
+++ src/ircd_lexer.c
@@ -418,7 +418,7 @@ char *yytext;
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
  *  USA
  *
  * $Id: ircd_lexer.l 20940 2005-10-12 22:58:03Z androsyn $
