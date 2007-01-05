a717d46697fb950b77d30e73b956a67de1cd5162
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2007-01-05 15:01:54 +0000

    Merge etrace into m_trace.c
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@23331 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_trace.c
+++ modules/m_trace.c
@@ -27,39 +27,68 @@
 #include "stdinc.h"
 #include "struct.h"
 #include "class.h"
-#include "hook.h"
 #include "client.h"
 #include "hash.h"
 #include "match.h"
-#include "ircd_lib.h"
 #include "ircd.h"
 #include "numeric.h"
 #include "s_serv.h"
 #include "s_conf.h"
+#include "hook.h"
 #include "send.h"
 #include "parse.h"
 #include "modules.h"
+#include "channel.h"
+#include "s_newconf.h"
+#include "s_log.h"
 
 static int m_trace(struct Client *, struct Client *, int, const char **);
+static int mo_etrace(struct Client *, struct Client *, int, const char **);
+static int me_etrace(struct Client *, struct Client *, int, const char **);
+static int mo_chantrace(struct Client *, struct Client *, int, const char **);
+static int mo_masktrace(struct Client *, struct Client *, int, const char **);
 
 static void trace_spy(struct Client *, struct Client *);
+
+
+struct Message chantrace_msgtab = {
+	"CHANTRACE", 0, 0, 0, MFLG_SLOW,
+	{mg_ignore, mg_not_oper, mg_ignore, mg_ignore, mg_ignore, {mo_chantrace, 2}}
+};
+
+struct Message masktrace_msgtab = {
+	"MASKTRACE", 0, 0, 0, MFLG_SLOW,
+	{mg_ignore, mg_not_oper, mg_ignore, mg_ignore, mg_ignore, {mo_masktrace, 2}}
+};
+                    
+struct Message etrace_msgtab = {
+	"ETRACE", 0, 0, 0, MFLG_SLOW,
+	{mg_ignore, mg_not_oper, mg_ignore, mg_ignore, {me_etrace, 0}, {mo_etrace, 0}}
+};
 
 struct Message trace_msgtab = {
 	"TRACE", 0, 0, 0, MFLG_SLOW,
 	{mg_unreg, {m_trace, 0}, {m_trace, 0}, mg_ignore, mg_ignore, {m_trace, 0}}
 };
 
+static void do_etrace(struct Client *source_p, int ipv4, int ipv6);
+static void do_etrace_full(struct Client *source_p);
+static void do_single_etrace(struct Client *source_p, struct Client *target_p);
+static void count_downlinks(struct Client *server_p, int *pservcount, int *pusercount);
+static int report_this_status(struct Client *source_p, struct Client *target_p, int dow);
+
+static const char *empty_sockhost = "255.255.255.255";
+static const char *spoofed_sockhost = "0";
+
 int doing_trace_hook;
 
-mapi_clist_av1 trace_clist[] = { &trace_msgtab, NULL };
+mapi_clist_av1 trace_clist[] = { &trace_msgtab, &etrace_msgtab, &chantrace_msgtab, &masktrace_msgtab, NULL };
 mapi_hlist_av1 trace_hlist[] = {
 	{ "doing_trace",	&doing_trace_hook },
 	{ NULL, NULL }
 };
 DECLARE_MODULE_AV1(trace, NULL, NULL, trace_clist, trace_hlist, NULL, "$Revision$");
 
-static void count_downlinks(struct Client *server_p, int *pservcount, int *pusercount);
-static int report_this_status(struct Client *source_p, struct Client *target_p, int dow);
 
 /*
  * m_trace
@@ -386,7 +415,7 @@ report_this_status(struct Client *source_p, struct Client *target_p,
 			
 			sendto_one_numeric(source_p, POP_QUEUE, tnumeric, form_str(tnumeric),
 					class_name, name,
-					show_ip(source_p, target_p) ? ip : "255.255.255.255",
+					show_ip(source_p, target_p) ? ip : empty_sockhost,
 					ircd_currenttime - target_p->localClient->lasttime,
 					ircd_currenttime - target_p->localClient->last);
 			cnt++;
@@ -437,3 +466,351 @@ trace_spy(struct Client *source_p, struct Client *target_p)
 
 	call_hook(doing_trace_hook, &hdata);
 }
+
+
+
+/*
+ * m_etrace
+ *      parv[0] = sender prefix
+ *      parv[1] = options [or target]
+ *	parv[2] = [target]
+ */
+static int
+mo_etrace(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
+{
+	if(parc > 1 && !EmptyString(parv[1]))
+	{
+		if(!irccmp(parv[1], "-full"))
+			do_etrace_full(source_p);
+#ifdef IPV6
+		else if(!irccmp(parv[1], "-v6"))
+			do_etrace(source_p, 0, 1);
+		else if(!irccmp(parv[1], "-v4"))
+			do_etrace(source_p, 1, 0);
+#endif
+		else
+		{
+			struct Client *target_p = find_named_person(parv[1]);
+
+			if(target_p)
+			{
+				if(!MyClient(target_p))
+					sendto_one(target_p, POP_QUEUE, ":%s ENCAP %s ETRACE %s",
+						get_id(source_p, target_p),
+						target_p->servptr->name,
+						get_id(target_p, target_p));
+				else
+					do_single_etrace(source_p, target_p);
+			}
+			else
+				sendto_one_numeric(source_p, POP_QUEUE, ERR_NOSUCHNICK,
+						form_str(ERR_NOSUCHNICK), parv[1]);
+		}
+	}
+	else
+		do_etrace(source_p, 1, 1);
+
+	return 0;
+}
+
+static int
+me_etrace(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
+{
+	struct Client *target_p;
+
+	if(!IsOper(source_p) || parc < 2 || EmptyString(parv[1]))
+		return 0;
+
+	/* we cant etrace remote clients.. we shouldnt even get sent them */
+	if((target_p = find_person(parv[1])) && MyClient(target_p))
+		do_single_etrace(source_p, target_p);
+
+	sendto_one_numeric(source_p, POP_QUEUE, RPL_ENDOFTRACE, form_str(RPL_ENDOFTRACE), 
+				target_p ? target_p->name : parv[1]);
+
+	return 0;
+}
+
+static void
+do_etrace(struct Client *source_p, int ipv4, int ipv6)
+{
+	struct Client *target_p;
+	dlink_node *ptr;
+
+	/* report all direct connections */
+	DLINK_FOREACH(ptr, lclient_list.head)
+	{
+		target_p = ptr->data;
+
+#ifdef IPV6
+		if((!ipv4 && target_p->localClient->ip.ss_family == AF_INET) ||
+		   (!ipv6 && target_p->localClient->ip.ss_family == AF_INET6))
+			continue;
+#endif
+
+		sendto_one(source_p, POP_QUEUE, form_str(RPL_ETRACE),
+			   me.name, source_p->name, 
+			   IsOper(target_p) ? "Oper" : "User", 
+			   get_client_class(target_p),
+			   target_p->name, target_p->username, target_p->host,
+			   show_ip(source_p, target_p) ? target_p->sockhost : empty_sockhost,
+			   target_p->info);
+	}
+
+	sendto_one_numeric(source_p, POP_QUEUE, RPL_ENDOFTRACE, form_str(RPL_ENDOFTRACE), me.name);
+}
+
+static void
+do_etrace_full(struct Client *source_p)
+{
+	dlink_node *ptr;
+
+	DLINK_FOREACH(ptr, lclient_list.head)
+	{
+		do_single_etrace(source_p, ptr->data);
+	}
+
+	sendto_one_numeric(source_p, POP_QUEUE, RPL_ENDOFTRACE, form_str(RPL_ENDOFTRACE), me.name);
+}
+
+/*
+ * do_single_etrace  - searches local clients and displays those matching
+ *                     a pattern
+ * input             - source client, target client
+ * output	     - etrace results
+ * side effects	     - etrace results are displayed
+ */
+static void
+do_single_etrace(struct Client *source_p, struct Client *target_p)
+{
+	/* note, we hide fullcaps for spoofed users, as mirc can often
+	 * advertise its internal ip address in the field --fl
+	 */
+	 if(!show_ip(source_p, target_p))
+		sendto_one(source_p, POP_QUEUE, form_str(RPL_ETRACEFULL),
+				me.name, source_p->name, 
+				IsOper(target_p) ? "Oper" : "User",
+				get_client_class(target_p),
+				target_p->name, target_p->username, target_p->host, 
+				empty_sockhost, "<hidden> <hidden>", target_p->info);
+	else
+		sendto_one(source_p, POP_QUEUE, form_str(RPL_ETRACEFULL),
+				me.name, source_p->name, 
+				IsOper(target_p) ? "Oper" : "User",
+				get_client_class(target_p),
+				target_p->name, target_p->username, 
+				target_p->host, target_p->sockhost,
+				target_p->localClient->fullcaps, target_p->info);
+}
+
+static int
+mo_chantrace(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
+{
+	struct Client *target_p;
+	struct Channel *chptr;
+	struct membership *msptr;
+	const char *sockhost;
+	const char *name;
+	dlink_node *ptr;
+	int operspy = 0;
+
+	name = parv[1];
+
+	if(IsOperSpy(source_p) && parv[1][0] == '!')
+	{
+		name++;
+		operspy = 1;
+		if(EmptyString(name))
+		{
+			sendto_one_numeric(source_p, POP_QUEUE, ERR_NEEDMOREPARAMS, form_str(ERR_NEEDMOREPARAMS), me.name, 
+					   source_p->name, "CHANTRACE");
+			return 0;
+		}
+	}
+
+	if((chptr = find_channel(name)) == NULL)
+	{
+		sendto_one_numeric(source_p, POP_QUEUE, ERR_NOSUCHCHANNEL, form_str(ERR_NOSUCHCHANNEL),
+				name);
+		return 0;
+	}
+
+	/* dont report operspys for nonexistant channels. */
+	if(operspy)
+		report_operspy(source_p, "CHANTRACE", chptr->chname);
+
+	if(!operspy && !IsMember(client_p, chptr))
+	{
+		sendto_one_numeric(source_p, POP_QUEUE, ERR_NOTONCHANNEL, form_str(ERR_NOTONCHANNEL),
+				chptr->chname);
+		return 0;
+	}
+
+	DLINK_FOREACH(ptr, chptr->members.head)
+	{
+		msptr = ptr->data;
+		target_p = msptr->client_p;
+
+		if(EmptyString(target_p->sockhost))
+			sockhost = empty_sockhost;
+		else if(!show_ip(source_p, target_p))
+			sockhost = spoofed_sockhost;
+		else
+			sockhost = target_p->sockhost;
+
+		sendto_one(source_p, HOLD_QUEUE, form_str(RPL_ETRACE),
+				me.name, source_p->name, 
+				IsOper(target_p) ? "Oper" : "User",
+				/* class field -- pretend its server.. */
+				target_p->servptr->name,
+				target_p->name, target_p->username, target_p->host,
+				sockhost, target_p->info);
+	}
+
+	sendto_one_numeric(source_p, POP_QUEUE, RPL_ENDOFTRACE, form_str(RPL_ENDOFTRACE), me.name); 
+	return 0;
+}
+
+static void
+match_masktrace(struct Client *source_p, dlink_list *list, const char *username, const char *hostname, const char *name, const char *gecos)
+{
+	struct Client *target_p;
+	dlink_node *ptr;
+	const char *sockhost;	
+	char *mangle_gecos = NULL;
+	
+	if(gecos != NULL)
+	{
+		if(strstr(gecos, "\\s"))
+		{
+			char *tmp = LOCAL_COPY(gecos);
+			char *orig = tmp;
+			char *new = tmp;
+	                while(*orig)
+        	        {
+				if(*orig == '\\' && *(orig + 1) != '\0')
+                       		{
+	                                if(*(orig + 1) == 's')
+	                                {
+	                                        *new++ = ' ';
+	                                        orig += 2;   
+	                                }
+	                                /* otherwise skip that and the escaped
+	                                 * character after it, so we dont mistake
+	                                 * \\s as \s --fl
+	                                 */
+	                                else
+	                                {   
+	                                        *new++ = *orig++;
+	                                        *new++ = *orig++;
+	                                }
+	                        }
+	                        else
+	                                *new++ = *orig++;
+	                }
+	
+	                *new = '\0';
+	                mangle_gecos = LOCAL_COPY(tmp);
+	        }
+	        else
+	                mangle_gecos = LOCAL_COPY(gecos);
+	}
+
+	DLINK_FOREACH(ptr, list->head)
+	{
+		target_p = ptr->data;
+		if(!IsPerson(target_p))
+			continue;
+		
+		if(EmptyString(target_p->sockhost))
+			sockhost = empty_sockhost;
+		else if(!show_ip(source_p, target_p))
+			sockhost = spoofed_sockhost;
+		else
+			sockhost = target_p->sockhost;
+
+		if(match(username, target_p->username) && (match(hostname, target_p->host) || 
+			 match(hostname, sockhost) || match_ips(hostname, sockhost)))
+		{
+			if(name != NULL && !match(name, target_p->name))
+				continue;
+
+			if(mangle_gecos != NULL && !match_esc(mangle_gecos, target_p->info))
+				continue;
+			
+			sendto_one(source_p, HOLD_QUEUE, form_str(RPL_ETRACE),
+				me.name, source_p->name, 
+				IsOper(target_p) ? "Oper" : "User",
+				/* class field -- pretend its server.. */
+				target_p->servptr->name,
+				target_p->name, target_p->username, target_p->host,
+				sockhost, target_p->info);
+		}
+	}
+}
+
+static int
+mo_masktrace(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
+{
+	char *name, *username, *hostname, *gecos;
+	const char *mask;
+	int operspy = 0;
+
+	mask = parv[1];	
+	name = LOCAL_COPY(parv[1]);	
+	collapse(name);
+
+	
+	if(IsOperSpy(source_p) && parv[1][0] == '!')
+	{
+		name++;
+		mask++;
+		operspy = 1;
+	}		
+	
+	if(parc > 2 && !EmptyString(parv[2]))
+	{
+		gecos = LOCAL_COPY(parv[2]);
+		collapse_esc(gecos);
+	} else
+		gecos = NULL;
+	
+
+	if((hostname = strchr(name, '@')) == NULL)
+	{
+		sendto_one(source_p, POP_QUEUE, ":%s NOTICE %s :Invalid parameters", me.name, source_p->name);
+		return 0;
+	}
+
+	*hostname++ = '\0';
+	
+	if((username = strchr(name, '!')) == NULL)
+	{
+		username = name;
+		name = NULL;
+	} else
+		*username++ = '\0';
+
+	if(EmptyString(username) || EmptyString(hostname))
+	{
+		sendto_one(source_p, POP_QUEUE, ":%s NOTICE %s :Invalid parameters", me.name, source_p->name);
+		return 0;
+	}
+			
+	if(operspy) {
+		char buf[512];
+		ircd_strlcpy(buf, mask, sizeof(buf));
+		if(!EmptyString(gecos)) {
+			ircd_strlcat(buf, " ", sizeof(buf));
+			ircd_strlcat(buf, gecos, sizeof(buf));
+		}		
+
+		report_operspy(source_p, "MASKTRACE", buf);	
+		match_masktrace(source_p, &global_client_list, username, hostname, name, gecos);		
+	} else
+		match_masktrace(source_p, &lclient_list, username, hostname, name, gecos);
+
+	sendto_one_numeric(source_p, POP_QUEUE, RPL_ENDOFTRACE, form_str(RPL_ENDOFTRACE), me.name); 
+	return 0;
+}
+
