4412a42c2ea2b2155aa5b326a066c2d042b70806
jilles <jilles@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2007-04-03 00:15:54 +0000

    Repair operspy who !#channel, broken by my ** fix.
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@23795 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_who.c
+++ modules/m_who.c
@@ -116,7 +116,7 @@ m_who(struct Client *client_p, struct Client *source_p, int parc, const char *pa
 	if(IsChannelName(mask))
 	{
 		/* List all users on a given channel */
-		chptr = find_channel(parv[1]);
+		chptr = find_channel(parv[1] + operspy);
 		if(chptr != NULL)
 		{
 			if(operspy)
@@ -128,7 +128,7 @@ m_who(struct Client *client_p, struct Client *source_p, int parc, const char *pa
 				do_who_on_channel(source_p, chptr, server_oper, NO);
 		}
 		sendto_one(source_p, POP_QUEUE, form_str(RPL_ENDOFWHO),
-			   me.name, source_p->name, parv[1]);
+			   me.name, source_p->name, parv[1] + operspy);
 		return 0;
 	}
 
