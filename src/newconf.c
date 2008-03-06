817f7cfe39d1917b64cdee4c667f8aec6db830d0
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2008-03-06 20:54:27 +0000

    keep newconf.c from bombing out when ipv6 isn't found
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@25093 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/newconf.c
+++ src/newconf.c
@@ -2172,7 +2172,9 @@ static struct conf_items conf_class_table[] =
 {
 	{ "ping_time", 		CF_TIME, conf_set_class_ping_time,			0, NULL },
 	{ "cidr_ipv4_bitlen",	CF_INT,  conf_set_class_cidr_ipv4_bitlen,		0, NULL },
+#ifdef RB_IPV6
 	{ "cidr_ipv6_bitlen",	CF_INT,  conf_set_class_cidr_ipv6_bitlen,		0, NULL },
+#endif
 	{ "number_per_cidr",	CF_INT,  conf_set_class_number_per_cidr,		0, NULL },
 	{ "number_per_ip",	CF_INT,  conf_set_class_number_per_ip,		0, NULL },
 	{ "number_per_ip_global",CF_INT, conf_set_class_number_per_ip_global,	0, NULL },
