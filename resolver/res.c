1a03e2eb50301ed19f27135adb0fc4506d12f1d5
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2008-07-12 00:51:28 +0000

    set resolver sockets non blocking
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@25723 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- resolver/res.c
+++ resolver/res.c
@@ -228,6 +228,7 @@ start_resolver(void)
 	if(res_fd_ipv4 == NULL)
 	{
 		res_fd_ipv4 = rb_socket(AF_INET, SOCK_DGRAM, 0, "UDP IPv4 resolver socket");
+		rb_set_nb(res_fd_ipv4);
 		if(res_fd_ipv4 != NULL)
 		{
 			rb_setselect(res_fd_ipv4, RB_SELECT_READ, res_readreply, NULL);
@@ -237,6 +238,7 @@ start_resolver(void)
 	if(res_fd_ipv6 == NULL)
 	{
 		res_fd_ipv6 = rb_socket(AF_INET6, SOCK_DGRAM, 0, "UDP IPv6 resolver socket");
+		rb_set_nb(res_fd_ipv6);
 		if(res_fd_ipv6 != NULL)
 		{
 			rb_setselect(res_fd_ipv6, RB_SELECT_READ, res_readreply, NULL);
