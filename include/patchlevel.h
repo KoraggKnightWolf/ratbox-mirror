c9091735575fc1351e05bd566951b4cc90e41f0c
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2008-08-18 23:48:52 +0000

    rev to beta11 and update RELNOTES
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@25907 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -25,7 +25,7 @@
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL   "ircd-ratbox-3.0.0beta10"
+#define PATCHLEVEL   "ircd-ratbox-3.0.0beta11"
 #define PATCHLEVEL_NUM	0300000102
 
 /* patchlevel numbers:
