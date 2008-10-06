4fd7b8ed4c2391cc2193268e3541c7deb109f0e1
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2008-10-06 13:54:40 +0000

    update patchlevel and release notes
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@26130 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -25,8 +25,8 @@
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL   "ircd-ratbox-3.0.0rc1"
-#define PATCHLEVEL_NUM	0300000201
+#define PATCHLEVEL   "ircd-ratbox-3.0.0rc2"
+#define PATCHLEVEL_NUM	0300000202
 
 /* patchlevel numbers:
  * <major><minor><point><status><statusnum>
