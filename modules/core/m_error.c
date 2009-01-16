d99bfe3d9ad947087c8626af8733719ecfb6f23f
jilles <jilles@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2009-01-16 23:43:33 +0000

    More helpful ERROR server notices: - Do not use hide_error_messages for certain "safe" ERRORs. - If hide_error_messages hides an ERROR from a handshake,   send a server notice anyway, but without the message   text. from charybdis (jilles) (not the entire commit, netwide snotes left out)
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@26415 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/core/m_error.c
+++ modules/core/m_error.c
@@ -26,6 +26,7 @@
 
 #include "stdinc.h"
 #include "struct.h"
+#include "ircd.h"
 #include "client.h"
 #include "send.h"
 #include "parse.h"
@@ -48,6 +49,36 @@ mapi_clist_av2 error_clist[] = {
 
 DECLARE_MODULE_AV2(error, NULL, NULL, error_clist, NULL, NULL, "$Revision$");
 
+/* Determine whether an ERROR message is safe to show (no IP address in it) */
+static int
+is_safe_error(const char *message)
+{
+	char prefix2[100];
+	const char *p;
+
+	if (!strncmp(message, "Closing Link: 127.0.0.1 (", 25))
+		return 1;
+	snprintf(prefix2, sizeof prefix2,
+			"Closing Link: 127.0.0.1 %s (", me.name);
+	if (!strncmp(message, prefix2, strlen(prefix2)))
+		return 1;
+	if (!strncmp(message, "Restart by ", 11))
+		return 1;
+	if (!strncmp(message, "Terminated by ", 14))
+		return 1;
+
+	if (!ircncmp(message, "Closing Link", 12))
+		return 0;
+	if (strchr(message, '['))
+		return 0;
+	p = strchr(message, '.');
+	if (p != NULL && p[1] != '\0')
+		return 0;
+	if (strchr(message, ':'))
+		return 0;
+
+	return 1;
+}
 
 /*
  * Note: At least at protocol level ERROR has only one parameter,
@@ -61,25 +92,24 @@ int
 m_error(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
 {
 	const char *para;
+	int hideit = ConfigFileEntry.hide_error_messages;
+
 	para = (parc > 1 && *parv[1] != '\0') ? parv[1] : "<>";
 
 	if(IsAnyServer(client_p))
 	{
+		if(is_safe_error(para))
+			hideit = 0;
 		ilog(L_SERVER, "Received ERROR message from %s: %s",
 		     log_client_name(source_p, SHOW_IP), para);
-	}
-
-	if(IsAnyServer(client_p) && ConfigFileEntry.hide_error_messages < 2)
-	{
-		sendto_realops_flags(UMODE_ALL, L_ADMIN,
-				     "ERROR :from %s -- %s",
-				     EmptyString(client_p->name) ? "" : client_p->name, para);
-
-		if(!ConfigFileEntry.hide_error_messages)
-			sendto_realops_flags(UMODE_ALL, L_OPER,
+		if (hideit < 2)
+			sendto_realops_flags(UMODE_ALL, hideit ? L_ADMIN : L_ALL,
 					     "ERROR :from %s -- %s",
-					     EmptyString(client_p->name) ? "" : client_p->name,
-					     para);
+					     EmptyString(client_p->name) ? "" : client_p->name, para);
+		if (hideit > 0)
+			sendto_realops_flags(UMODE_ALL, hideit == 1 ? L_OPER : L_ALL,
+					     "ERROR :from %s -- <hidden>",
+					     EmptyString(client_p->name) ? "" : client_p->name);
 	}
 
 	exit_client(client_p, source_p, source_p, "ERROR");
@@ -91,18 +121,21 @@ static int
 ms_error(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
 {
 	const char *para;
+	int hideit = ConfigFileEntry.hide_error_messages;
 
 	para = (parc > 1 && *parv[1] != '\0') ? parv[1] : "<>";
 
 	ilog(L_SERVER, "Received ERROR message from %s: %s",
 	     log_client_name(source_p, SHOW_IP), para);
 
-	if(ConfigFileEntry.hide_error_messages == 2)
+	if(is_safe_error(para))
+		hideit = 0;
+	if(hideit == 2)
 		return 0;
 
 	if(client_p == source_p)
 	{
-		sendto_realops_flags(UMODE_ALL, L_ADMIN, "ERROR :from %s -- %s",
+		sendto_realops_flags(UMODE_ALL, hideit ? L_ADMIN : L_ALL, "ERROR :from %s -- %s",
 				     client_p->name, para);
 
 		if(!ConfigFileEntry.hide_error_messages)
@@ -111,13 +144,8 @@ ms_error(struct Client *client_p, struct Client *source_p, int parc, const char 
 	}
 	else
 	{
-		sendto_realops_flags(UMODE_ALL, L_ADMIN, "ERROR :from %s via %s -- %s",
+		sendto_realops_flags(UMODE_ALL, hideit ? L_ADMIN : L_ALL, "ERROR :from %s via %s -- %s",
 				     source_p->name, client_p->name, para);
-
-		if(!ConfigFileEntry.hide_error_messages)
-			sendto_realops_flags(UMODE_ALL, L_OPER,
-					     "ERROR :from %s via %s -- %s",
-					     source_p->name, client_p->name, para);
 	}
 
 	return 0;
