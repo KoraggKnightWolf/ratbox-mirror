2b18a528eb586b7504fbece6d79eb67754a67980
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2015-08-21 15:47:06 +0000

    move struct operhash_entry to be internal to src/operhash.c
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@28645 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/operhash.h
+++ include/operhash.h
@@ -8,13 +8,6 @@
 #ifndef INCLUDED_operhash_h
 #define INCLUDED_operhash_h
 
-
-struct operhash_entry
-{
-	char *name;
-	int refcount;
-};
-
 void init_operhash(void);
 const char *operhash_add(const char *name);
 void operhash_delete(const char *name);
