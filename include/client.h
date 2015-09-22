0895c227e76729b926391ef3dc7e45a0e1569845
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2015-09-22 17:24:10 +0000

    Change status values to enums from #defines It also appears that the status field is NOT a bitmask, so there isnt a reason to have values based on the bits.
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/trunk@28657 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/client.h
+++ include/client.h
@@ -30,16 +30,19 @@
 #define PASSWDLEN	128
 
 /*
- * status macros.
+ * status enums.
  */
-#define STAT_CONNECTING		0x01
-#define STAT_HANDSHAKE		0x02
-#define STAT_ME			0x04
-#define STAT_UNKNOWN		0x08
-#define STAT_REJECT		0x10
-#define STAT_SERVER		0x20
-#define STAT_CLIENT		0x40
 
+
+enum {
+	STAT_CONNECTING,
+	STAT_HANDSHAKE,
+	STAT_ME,
+	STAT_UNKNOWN,
+	STAT_REJECT,
+	STAT_SERVER,
+	STAT_CLIENT,
+};
 
 #define IsRegisteredUser(x)	((x)->status == STAT_CLIENT)
 #define IsRegistered(x)		(((x)->status  > STAT_UNKNOWN) && ((x)->status != STAT_REJECT))
@@ -79,12 +82,6 @@
 					OPER_HANDLER : CLIENT_HANDLER; }
 #define SetRemoteClient(x)	{(x)->status = STAT_CLIENT; \
 				 (x)->handler = RCLIENT_HANDLER; }
-
-#define STAT_CLIENT_PARSE (STAT_UNKNOWN | STAT_CLIENT)
-#define STAT_SERVER_PARSE (STAT_CONNECTING | STAT_HANDSHAKE | STAT_SERVER)
-
-#define PARSE_AS_CLIENT(x)	((x)->status & STAT_CLIENT_PARSE)
-#define PARSE_AS_SERVER(x)	((x)->status & STAT_SERVER_PARSE)
 
 
 /*
